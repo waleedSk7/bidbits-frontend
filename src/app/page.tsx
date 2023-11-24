8546712c3617bf02e8d12de211c093a8cfedff68
Manas Ashwin <manaschubby@gmail.com>
2023-11-24 13:37:48 +0530

    Init Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+import Navbar from "@/components/Navbar";
+import { Button } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="flex flex-col min-h-[100vh]">
+				<Navbar />
+				<main className="flex-1">
+					<section className="w-full py-12 sm:py-24 md:py-32 xl:py-48">
+						<div className="container px-4 md:px-6">
+							<div className="flex flex-col items-center space-y-4 text-center">
+								<h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+									Welcome to BID BITS
+								</h1>
+								<p className="mx-auto max-w-[700px] text-zinc-500 md:text-xl dark:text-zinc-400">
+									The best place for BPHC students to auction their products and
+									find great deals.
+								</p>
+								<Link
+									className="inline-flex h-10 items-center justify-center rounded-md bg-zinc-900 px-8 text-sm font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300"
+									href="#"
+								>
+									Start Auctioning
+								</Link>
+							</div>
+						</div>
+					</section>
+					<section className="w-full py-12 md:py-24 lg:py-32">
+						<div className="container px-4 md:px-6">
+							<h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-center">
+								Featured Products
+							</h2>
+							<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+								<div className="rounded-md shadow-md overflow-hidden">
+									<img
+										alt="Product"
+										className="object-cover"
+										height="300"
+										src="https://i.pcmag.com/imagery/reviews/04jlYF4fsF2M5cejiU8lw7G-2.fit_lim.size_840x473.v1685028816.jpg"
+										style={{
+											aspectRatio: "400/300",
+											objectFit: "cover",
+										}}
+										width="400"
+									/>
+									<div className="p-4">
+										<h3 className="text-lg font-bold">Product Name</h3>
+										<p className="text-sm text-gray-500 mt-2">
+											Short product description goes here.
+										</p>
+										<Button className="mt-4">View Details</Button>
+									</div>
+								</div>
+								<div className="rounded-md shadow-md overflow-hidden">
+									<img
+										alt="Product"
+										className="object-cover"
+										height="300"
+										src="https://i.pcmag.com/imagery/reviews/04jlYF4fsF2M5cejiU8lw7G-2.fit_lim.size_840x473.v1685028816.jpg"
+										style={{
+											aspectRatio: "400/300",
+											objectFit: "cover",
+										}}
+										width="400"
+									/>
+									<div className="p-4">
+										<h3 className="text-lg font-bold">Product Name</h3>
+										<p className="text-sm text-gray-500 mt-2">
+											Short product description goes here.
+										</p>
+										<Button className="mt-4">View Details</Button>
+									</div>
+								</div>
+								<div className="rounded-md shadow-md overflow-hidden">
+									<img
+										alt="Product"
+										className="object-cover"
+										height="300"
+										src="https://i.pcmag.com/imagery/reviews/04jlYF4fsF2M5cejiU8lw7G-2.fit_lim.size_840x473.v1685028816.jpg"
+										style={{
+											aspectRatio: "400/300",
+											objectFit: "cover",
+										}}
+										width="400"
+									/>
+									<div className="p-4">
+										<h3 className="text-lg font-bold">Product Name</h3>
+										<p className="text-sm text-gray-500 mt-2">
+											Short product description goes here.
+										</p>
+										<Button className="mt-4">View Details</Button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</main>
+				<footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+					<p className="text-xs text-zinc-500 dark:text-zinc-400">
+						© College Auctions. All rights reserved.
+					</p>
+					<nav className="sm:ml-auto flex gap-4 sm:gap-6">
+						<Link
+							className="text-xs hover:underline underline-offset-4"
+							href="#"
+						>
+							About Us
+						</Link>
+						<Link
+							className="text-xs hover:underline underline-offset-4"
+							href="#"
+						>
+							Contact
+						</Link>
+						<Link
+							className="text-xs hover:underline underline-offset-4"
+							href="#"
+						>
+							Privacy Policy
+						</Link>
+						<Link
+							className="text-xs hover:underline underline-offset-4"
+							href="#"
+						>
+							Terms of Service
+						</Link>
+					</nav>
+				</footer>
+			</div>
+		</main>
+	);
 }
