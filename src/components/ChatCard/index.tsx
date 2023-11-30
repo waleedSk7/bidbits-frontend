1aa256cc29b3cd77b2f7df080b2bd7e24c808802
Manas Ashwin <manaschubby@gmail.com>
2023-11-30 17:29:19 +0530

    fix routing

--- src/components/ChatCard/index.tsx
+++ src/components/ChatCard/index.tsx
@@ -5,6 +5,7 @@ export interface IChatCardProfileProps {
 	chat: ProfileChat["userMessages"][1];
 	product: ProfileChat["product"];
 	name: string;
+	userId: string;
 }
 
 export default function ChatCardProfile(props: IChatCardProfileProps) {
@@ -13,7 +14,8 @@ export default function ChatCardProfile(props: IChatCardProfileProps) {
 		<div
 			className="card rounded-lg border overflow-hidden min-w-max"
 			onClick={() =>
-				(window.location.href = "/chat/" + props.product.productId)
+				(window.location.href =
+					"/chat/" + props.product.productId + "/" + props.userId)
 			}
 		>
 			<div className="flex items-center gap-4 p-4">
